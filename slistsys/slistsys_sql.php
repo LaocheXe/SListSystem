6ccee7c8538a50819f7a208634e28a78112fea77
LaocheXe <LaocheXe@users.noreply.github.com>
2019-09-07 11:47:34 -0500

    Start
    Start

--- slistsys/slistsys_sql.php
+++ slistsys/slistsys_sql.php
@@ -1,12 +1,14 @@
 <?php exit;?>
-
 CREATE TABLE IF NOT EXISTS `serverlist` (
   `server_id` int(10) NOT NULL AUTO_INCREMENT,
   `servercat_id` smallint(5) UNSIGNED NOT NULL DEFAULT '0',
   `server_name` varchar(250) NOT NULL default '',
-  `server_ip` int(10) unsigned NOT NULL default '',
-  `server_port` int(10) unsigned NOT NULL default '',
-  `viewer_cc` text,
+  `server_ip` text,
+  `server_port` text,
+  `server_pass` text,
+  `server_gsid` text,
+  `server_enable_vcc` int(12) unsigned NOT NULL,
+  `server_viewer_cc` text,
   PRIMARY KEY (`server_id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=1;
 
@@ -21,6 +23,7 @@ CREATE TABLE IF NOT EXISTS `serverlist_submission` (
   `s_game` varchar(250) NOT NULL default '',
   `s_ip` text,
   `s_port` text,
+  `s_pass` text,
   `s_gsid` text,
   PRIMARY KEY (`submit_id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=1;
